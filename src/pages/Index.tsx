2ad3e3717f29c476f5cff7f4e0a2b51e08ca49b0
Nitik Sharma <nitiksharma.job@gmail.com>
2024-06-30 09:43:07 +0530

    feat: Profile Updated

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,9 @@
 import Navbar from './main/Navbar'
 
+// progress bar for table has to be added
+// profile page not yet made
+// url based pagination not done
+
 const Index = ({ children }: { children: JSX.Element }) => {
   return (
     <>
--- src/pages/main/layouts/Profile/Profile.tsx
+++ src/pages/main/layouts/Profile/Profile.tsx
@@ -1,83 +1,87 @@
+import { Link } from 'react-router-dom'
 import users from '../../../../data/users.json'
-// {
-//   "id": 2,
-//   "name": "Ervin Howell",
-//   "username": "Antonette",
-//   "email": "Shanna@melissa.tv",
-//   "address": {
-//       "street": "Victor Plains",
-//       "suite": "Suite 879",
-//       "city": "Wisokyburgh",
-//       "zipcode": "90566-7771",
-//       "geo": {
-//           "lat": "-43.9509",
-//           "lng": "-34.4618"
-//       }
-//   },
-//   "phone": "010-692-6593 x09125",
-//   "website": "anastasia.net",
-//   "company": {
-//       "name": "Deckow-Crist",
-//       "catchPhrase": "Proactive didactic contingency",
-//       "bs": "synergize scalable supply-chains"
-//   }
-// }
+import { FaArrowLeft } from 'react-icons/fa'
 interface UserType {
   name: string
   email: string
   phone: string
   id: number
+  address: {
+    street: string
+    suite: string
+    city: string
+  }
 }
 const Profile = () => {
-  const { id, name, email, phone }: UserType = users[1]
-  console.log(users[1])
+  const { id, name, email, phone, address }: UserType = users[1]
   return (
-    <div>
-      <div>
-        <div className='avatar placeholder'>
-          <div className='bg-neutral text-neutral-content w-24 rounded-full'>
-            <span className='text-3xl'>{name[0]}</span>
+    <div className='mx-60 p-4 flex flex-col gap-4 h-full'>
+      <p className='flex items-center gap-4'>
+        <Link to='/'>
+          <FaArrowLeft />
+        </Link>
+        Welcome, {name}
+      </p>
+      <section className='border-4 border-slate-100 p-8 h-full flex flex-col gap-4 '>
+        <section className='flex items-center gap-2'>
+          <div className='avatar placeholder'>
+            <div className='bg-neutral text-neutral-content w-24 rounded-full'>
+              <span className='text-3xl'>{name[0]}</span>
+            </div>
           </div>
-          <div>
-            <p>{name}</p>
-            <p>{email}</p>
+          <div className='flex flex-col'>
+            <p className='font-bold text-2xl'>{name}</p>
+            <p className='text-gray-400'>{email}</p>
           </div>
-        </div>
-      </div>
-      <div>
-        <div>
-          <input
-            type='text'
-            placeholder={String(id)}
-            className='input input-bordered w-full max-w-xs font-black'
-            disabled
-          />
-        </div>
-        <div>
-          <input
-            type='text'
-            placeholder={name}
-            className='input input-bordered w-full max-w-xs'
-            disabled
-          />
-        </div>
-        <div>
-          <input
-            type='text'
-            placeholder={email}
-            className='input input-bordered w-full max-w-xs'
-            disabled
-          />
-        </div>
-        <div>
-          <input
-            type='text'
-            placeholder={phone}
-            className='input input-bordered w-full max-w-xs font-black'
-            disabled
-          />
-        </div>
-      </div>
+        </section>
+        <section className='grid lg:grid-cols-2 grid-cols-1 gap-4'>
+          <div className='flex flex-col gap-2'>
+            <label>Post ID</label>
+            <input
+              type='text'
+              placeholder={String(id)}
+              className='input input-bordered w-full font-black'
+              disabled
+            />
+          </div>
+          <div className='flex flex-col gap-2'>
+            <label>Name</label>
+            <input
+              type='text'
+              placeholder={name}
+              className='input input-bordered w-full font-black'
+              disabled
+            />
+          </div>
+          <div className='flex flex-col gap-2'>
+            <label>Email</label>
+            <input
+              type='text'
+              placeholder={email}
+              className='input input-bordered w-full font-black'
+              disabled
+            />
+          </div>
+          <div className='flex flex-col gap-2'>
+            <label>Address</label>
+            <input
+              type='text'
+              placeholder={`${address.suite} ${address.street} ${address.city}`}
+              className='input input-bordered w-full font-black'
+              disabled
+            />
+          </div>
+          <div className='flex flex-col gap-2'>
+            <label>Phone</label>
+            <input
+              type='text'
+              placeholder={phone}
+              className='input input-bordered w-full font-black'
+              disabled
+            />
+          </div>
+        </section>
+      </section>
     </div>
   )
 }
